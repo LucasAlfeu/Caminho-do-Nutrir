cd83e5e9d39b595f80da2edf61b4031b863b9af1
Lucas Alfeu <lucas.a1feu19@gmail.com>
2025-12-10 23:09:48 -0300

    Adiconando um marcador

--- Desenvolvimento/caminho-nutrir/src/app/components/mapa-leaflet/mapa-leaflet.ts
+++ Desenvolvimento/caminho-nutrir/src/app/components/mapa-leaflet/mapa-leaflet.ts
@@ -18,9 +18,17 @@ import * as L from 'leaflet';
 export class MapaLeaflet implements AfterViewInit { 
   private map!: L.Map;
 
+  customIcon = L.icon({
+  iconUrl: 'images/pino-de-localizacao.png',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32]
+});
+
   ngAfterViewInit(): void {
     console.log('Iniciando mapa...');
     this.initMap();
+    this.addMarker(-22.7414, -43.7053, "Esse é um teste")
   }
 
   private initMap(): void {
@@ -40,5 +48,34 @@ export class MapaLeaflet implements AfterViewInit {
     setTimeout(() => {
       this.map.invalidateSize();
     }, 100);
+  }
+
+   private fixLeafletIcon(): void {
+    const iconRetinaUrl = 'images/pino-de-localizacao.png';
+    const iconUrl = 'images/pino-de-localizacao.png';
+    const shadowUrl = 'images/pino-de-localizacao.png';
+    
+    const iconDefault = L.icon({
+      iconRetinaUrl,
+      iconUrl,
+      shadowUrl,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      tooltipAnchor: [16, -28],
+      shadowSize: [41, 41]
+    });
+    
+    L.Marker.prototype.options.icon = iconDefault;
+  }
+
+  private addMarker(lat: number, lng: number, mensagem?: string): L.Marker {
+    const marker = L.marker([lat, lng], {icon: this.customIcon}).addTo(this.map);
+    
+    if (mensagem) {
+      marker.bindPopup(mensagem);
+    }
+    
+    return marker;
   }
 }
