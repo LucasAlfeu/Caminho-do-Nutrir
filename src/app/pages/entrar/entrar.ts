357e10b62cb6b6712d9dc7fc1fa197ac8b33a13b
Lucas Alfeu <a1feu@outlook.com>
2026-05-19 09:42:34 -0300

    FEAT(entrar): Preparando objeto para ser enviado a api/

--- src/app/pages/entrar/entrar.ts
+++ src/app/pages/entrar/entrar.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
-import { HeaderSecundario } from '../../components/header-secundario/header-secundario';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-entrar',
-  imports: [
-    HeaderSecundario
-  ],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './entrar.html',
   styleUrl: './entrar.css',
 })
 export class Entrar {
 
+  form!: FormGroup;
+
+  constructor(
+    protected fb: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.createForm()
+  }
+
+  createForm(){
+    this.form = this.fb.group({
+      email: ['', Validators.compose([Validators.required])],
+      senha: ['', Validators.compose([Validators.required])]
+    })
+  }
+
+  enviarCredenciais(){
+    if(!this.form){
+      console.log("Não possui o form")
+      return
+    }
+    const credenciais = {
+      email: this.form.get('email')?.value,
+      senha: this.form.get('senha')?.value
+    }
+
+    console.log(credenciais)
+  }
 }
