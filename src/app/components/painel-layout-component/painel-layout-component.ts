c5a63a879e6d9d4504eb46ea025cf1379591e031
Lucas Alfeu <a1feu@outlook.com>
2026-05-28 14:58:46 -0300

    FEAT: menu lateral, reformulando

--- src/app/components/painel-layout-component/painel-layout-component.ts
+++ src/app/components/painel-layout-component/painel-layout-component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuLateal } from '../../components/menu-lateal/menu-lateal';
-import { HeaderSecundario } from '../../components/header-secundario/header-secundario';
 import { FooterSecundario } from '../../components/footer-secundario/footer-secundario';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-painel-layout-component',
   imports: [
-    HeaderSecundario,
     MenuLateal,
     FooterSecundario,
     RouterOutlet,
