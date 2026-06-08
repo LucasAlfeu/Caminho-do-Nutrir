3309511490acfaeb80b80515d2b9d6546e691a92
Lucas Alfeu <a1feu@outlook.com>
2026-06-08 11:14:48 -0300

    FEAT: Ajuste na estilização

--- src/app/pages/painel/painel.ts
+++ src/app/pages/painel/painel.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-painel',
@@ -8,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class Painel {
   nomeUsuario: string = 'Lucas Alfeu da Silva Oliveira'
+
+  constructor(private router: Router) {}
+
+  goTo(){
+    this.router.navigate(['painel/cadastrar-banco']);
+  }
 }
