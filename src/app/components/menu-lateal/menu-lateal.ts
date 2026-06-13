107df2607a680b6be433f2f4211cfe03e9f60090
Lucas Alfeu <a1feu@outlook.com>
2026-06-13 13:10:01 -0300

    FEAT: Desconexão do sistema

--- src/app/components/menu-lateal/menu-lateal.ts
+++ src/app/components/menu-lateal/menu-lateal.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-lateal',
@@ -8,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class MenuLateal {
 
+  constructor(private router: Router) { }
+
+  sair(){
+    localStorage.clear();
+    this.router.navigate(['/entrar']);
+  }
 }
