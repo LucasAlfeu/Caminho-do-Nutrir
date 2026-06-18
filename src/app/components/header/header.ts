0e19e4091331a6e62b66664790bf104659d4d4e7
Lucas Alfeu <a1feu@outlook.com>
2026-06-18 14:40:56 -0300

    FEAT: opção de ir para o painel quando o usuário estiver ligado

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AutenticacaoService } from '../../services/Autenticacao/autenticacao-service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
 
+  constructor(
+    private autenticacaoService: AutenticacaoService,
+  ) { }
+
+  validaUsuarioLogado(){
+    return this.autenticacaoService.validaUsuarioLogado()
+  }
 }
