6b016b32a3801667ae467224c0aae29321aa7198
Lucas Alfeu <a1feu@outlook.com>
2026-07-22 18:19:01 -0300

    FEAT(modal-categoria): Adicionando o formulário para a criação de uma categoria

--- src/app/components/modal-categoria/modal-categoria.ts
+++ src/app/components/modal-categoria/modal-categoria.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastrService } from 'ngx-toastr';
+import Pickr from '@simonwep/pickr';
+
 
 declare var bootstrap: any;
 
@@ -11,14 +13,57 @@ declare var bootstrap: any;
   styleUrl: './modal-categoria.css',
 })
 export class ModalCategoria implements AfterViewInit {
+  @ViewChild('colorPicker', { static: false }) colorPickerElement!: ElementRef;
   @ViewChild('meuModal') modalElement!: ElementRef;
   private modalInstance: any;
+
+  private pickrInstance: any;
+  public corSelecionada: string = '#ff0000';
 
   constructor(protected toastrService: ToastrService) { }
 
   ngAfterViewInit(): void {
     if (this.modalElement) {
       this.modalInstance = new bootstrap.Modal(this.modalElement.nativeElement);
+    }
+
+    this.pickrInstance = Pickr.create({
+      el: this.colorPickerElement.nativeElement,
+      theme: 'nano',
+      container: this.colorPickerElement.nativeElement.parentElement,
+      default: this.corSelecionada,
+
+      components: {
+        preview: true,
+        opacity: false,
+        hue: true,
+        interaction: {
+          hex: true,
+          rgba: false,
+          cmyk: false,
+          input: true,
+          clear: false,
+          save: true
+        }
+      },
+      i18n: {
+        'btn:save': 'Salvar',
+      }
+    });
+
+    this.pickrInstance.on('change', (color: any) => {
+      this.corSelecionada = color.toHEXA().toString();
+    });
+
+    this.pickrInstance.on('save', (color: any) => {
+      this.corSelecionada = color.toHEXA().toString();
+      this.pickrInstance.hide();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.pickrInstance) {
+      this.pickrInstance.destroyAndRemove();
     }
   }
 
