eda261b93443445de9ddc721d50a4a6952afeafc
Lucas Alfeu <a1feu@outlook.com>
2026-07-22 21:53:30 -0300

    FEAT(categoria): Integração do cadastro de uma categoria com oa API

--- src/app/pages/categorias/categorias.ts
+++ src/app/pages/categorias/categorias.ts
@@ -3,6 +3,7 @@ import { CategoriaService } from '../../services/Categoria/categoria';
 import { Categoria } from '../../class/Categoria';
 import { CommonModule } from '@angular/common';
 import { ModalCategoria } from '../../components/modal-categoria/modal-categoria';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-categorias',
@@ -16,12 +17,11 @@ export class Categorias implements OnInit {
   listCategorias: Categoria[] = [];
   totalCategoria: string = '';
 
-  constructor( private categoriaService: CategoriaService) {
+  constructor( private categoriaService: CategoriaService, private toastr: ToastrService,) {
 
   }
 
   ngOnInit() {
-    console.log("Iniciou")
     this.buscarCategorias();
   }
 
@@ -41,7 +41,22 @@ export class Categorias implements OnInit {
     this.modalCategoria.abrirModal()
   }
 
-  salvar(e: any){
-    console.log("Componente pai", e)
+  salvar(dados: any){
+    console.log("Componente pai", dados)
+
+    this.categoriaService.cadastrarCategoria(dados).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.buscarCategorias();
+        this.toastr.success("Categoria cadastrada com sucesso", "Sucesso")
+      },
+      error: (err) => {
+        if(err.error && err.error.errors && err.error.errors.default) {
+          this.toastr.error(err.error.errors.default, "Erro")
+          return;
+        }
+        this.toastr.error("Não foi possível cadastrar")
+      }
+    })
   }
 }
--- src/app/services/Categoria/categoria.ts
+++ src/app/services/Categoria/categoria.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Categoria } from '../../class/Categoria';
 import { environment } from '../../environments/environment';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,19 @@ export class CategoriaService {
     return this.http.get<Categoria[]>(`${environment.apiUrl}/classificacao`, {
       observe: 'response'
     })
+  }
+
+  cadastrarCategoria(dados: any){
+    const aux = localStorage.getItem('usuario');
+
+    if (aux != null) {
+      const user = JSON.parse(aux);
+
+      const headers = new HttpHeaders({
+        'Authorization': `Bearer ${user.accessToken}`
+      });
+      return this.http.post<Categoria>(`${environment.apiUrl}/classificacao`, dados, { headers })
+    }
+    return throwError(() => new Error('Usuário não autenticado.'));
   }
 }
